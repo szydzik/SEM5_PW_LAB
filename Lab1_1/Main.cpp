e4ba957a0589fd7b882ac150ec20a1461c8ab924
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-10-19 21:50:17 +0200

    Lab1 - Lab1_1 - Bug fixes

--- Lab1_1/Main.cpp
+++ Lab1_1/Main.cpp
@@ -34,17 +34,15 @@ int quadraticFunction(double a, double b, double c, double &x1, double &x2) {
 			x1 = 0;
 			return 1;
 		}
-		//{	+,	0,	+	}
-		//{	-,	0,	-	}
-		if ((a > 0 && c > 0) || (a < 0 && c < 0)) {
-			return 3;
-		}
-		//{	+,	0,	-	}
-		//{	-,	0,	+	}
-		if ((a > 0 && c < 0) || (a < 0 && c > 0)) {
-			x1 = sqrt(c / a);
-			x2 = -sqrt(c / a);
+
+		//{	+/-,	0,	+/-	}
+		if ((-1.0 * c / a) >= 0) {
+			x1 = sqrt(-1.0 * c / a);
+			x2 = -sqrt(-1.0 * c / a);
 			return 2;
+		}
+		else {
+			return -1;
 		}
 	}
 	if (a != 0) {
@@ -77,47 +75,50 @@ int main()
 	setlocale(LC_ALL, "");
 
 	double a, b, c;
-	cout << "podaj a:" << endl;
-	cin >> a;
-	cout << "podaj b:" << endl;
-	cin >> b;
-	cout << "podaj c:" << endl;
-	cin >> c;
+	for (;;) {
+		system("cls");
+		cout << "podaj a:" << endl;
+		cin >> a;
+		cout << "podaj b:" << endl;
+		cin >> b;
+		cout << "podaj c:" << endl;
+		cin >> c;
 
-	double x1 = 0;
-	double x2 = 0;
+		double x1 = 0;
+		double x2 = 0;
 
-	int results = quadraticFunction(a, b, c, x1, x2);
+		int results = quadraticFunction(a, b, c, x1, x2);
 
-	switch (results) {
-	case -1: {
-		cout << "Równanie sprzeczne" << endl;
-		break;
-	}
-	case 0: {
-		cout << "Brak rozwi¹zañ rzeczywistych" << endl;
-		break;
-	}
-	case 1: {
-		cout << "Jedno rozwi¹zanie" << endl
-			<< "x1 = " << x1 << endl;
-		break;
-	}
-	case 2: {
-		cout << "Dwa rozwi¹zania: " << endl
-			<< "x1 = " << x1 << endl
-			<< "x2 = " << x2 << endl;
-		break;
-	}
-	case 3: {
-		cout << "Rozwi¹zaniami s¹ wszystkie liczby rzeczywiste" << endl;
-		break;
-	}
-	default: {
-		cout << "Wyst¹pi³ nieoczekiwany b³¹d" << endl;
-	}
-	}
+		switch (results) {
+		case -1: {
+			cout << "Równanie sprzeczne" << endl;
+			break;
+		}
+		case 0: {
+			cout << "Brak rozwi¹zañ rzeczywistych" << endl;
+			break;
+		}
+		case 1: {
+			cout << "Jedno rozwi¹zanie" << endl
+				<< "x1 = " << x1 << endl;
+			break;
+		}
+		case 2: {
+			cout << "Dwa rozwi¹zania: " << endl
+				<< "x1 = " << x1 << endl
+				<< "x2 = " << x2 << endl;
+			break;
+		}
+		case 3: {
+			cout << "Rozwi¹zaniami s¹ wszystkie liczby rzeczywiste" << endl;
+			break;
+		}
+		default: {
+			cout << "Wyst¹pi³ nieoczekiwany b³¹d" << endl;
+		}
+		}
 
-	system("pause");
+		system("pause");
+	}
 	return 0;
 }
---
+++ Lab1_1/Main.h
@@ -0,0 +1,2 @@
+double calculateDelta(double a, double b, double c);
+int quadraticFunction(double a, double b, double c, double & x1, double & x2);
